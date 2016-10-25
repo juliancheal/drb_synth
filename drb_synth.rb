523eaf9c0662019fae98571ac9be437005aa77c4
Julian Cheal <jcheal@redhat.com>
2016-10-25 19:25:13 +0100

    PAUSED: Use `git resume` to continue working.

--- drb_synth.rb
+++ drb_synth.rb
@@ -3,6 +3,7 @@ require 'midi'
 require 'yaml'
 require 'ostruct'
 require 'logger'
+require './endpoints'
 
 class DRBSynth
 
@@ -15,12 +16,9 @@ class DRBSynth
     @log = Logger.new(STDOUT)
     @log.level = Logger::INFO
 
-    configuration
-
-    @synths.each do |synth|
-      device = MIDI::Session.new(all_devices.find { |d| d.id == synth.port }) unless synth.port.nil?
-      @devices[synth.key.to_sym] = {device: device, metadata: synth}
-    end
+    @instructions = {
+      endpoints: Endpoints.new
+    }
   end
 
   def configuration(file='synths.yml')
@@ -30,6 +28,11 @@ class DRBSynth
       synth_file.each do |synth|
         @synths << OpenStruct.new(synth)
       end
+    end
+
+    @synths.each do |synth|
+      device = MIDI::Session.new(all_devices.find { |d| d.id == synth.port }) unless synth.port.nil?
+      @devices[synth.key.to_sym] = {device: device, metadata: synth}
     end
   end
 
@@ -51,6 +54,29 @@ class DRBSynth
 
   def list_devices(&block)
     all_devices
+  end
+
+  def method_missing(method, *args, &block)
+    results = delegate(method, args, &block)
+    if results.empty?
+      super
+    else
+      messages = results.map do |result|
+        result[:message]
+      end
+      messages.compact.first
+    end
+  end
+
+  def delegate(method, args, &block)
+    results = @instructions.map do |key, instruction|
+    message = instruction.send(method, *args, &block)
+    {
+      instruction_type: key,
+      message: message
+    }
+    end
+    results.compact
   end
 
   private
